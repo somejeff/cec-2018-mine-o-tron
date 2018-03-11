24a18b04f8f5a69390584b1282564b554a2fe57b
Jeffrey Lee <somejeff@gmail.com>
2018-03-11 14:20:28 -0400

    #6 added table to show hubs in console, that'll tell us if there's ore there.  Also moved to a spot that actually has ore.

--- mine-o-tron.js
+++ mine-o-tron.js
@@ -8,7 +8,15 @@ $.getJSON("startup?token=3efbdfd5be1d284d8b3dd660cc31f839", function (data) {
             // deploy hub to 2000
             $.getJSON("deploy_hubs?token=3efbdfd5be1d284d8b3dd660cc31f839&hubs=H1&sector_ids=2000", function (data) {
                 // you should see the hub appear
+                console.log("Deployed Hub H1");
             });
         }, 3000); // 3 second delay before calling deploy
     });
 });
+
+window.setInterval(function () {
+    $.getJSON("status_report?token=3efbdfd5be1d284d8b3dd660cc31f839", function (data) {
+        console.log(data.status_report.team.week);
+        console.table(data.status_report.hubs);
+    });
+},2000);
