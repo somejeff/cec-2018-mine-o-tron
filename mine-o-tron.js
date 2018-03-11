e158a661de9ae4cc780b0ba64f069bb6681282a1
Jeffrey Lee <somejeff@gmail.com>
2018-03-11 14:46:20 -0400

    #7 Repeatedly shipping ore, turns out we only pay by tonnage, but each call

--- mine-o-tron.js
+++ mine-o-tron.js
@@ -8,20 +8,25 @@ $.getJSON("startup?token=3efbdfd5be1d284d8b3dd660cc31f839", function (data) {
             // deploy hub to 2000
             $.getJSON("prospect_report?token=3efbdfd5be1d284d8b3dd660cc31f839", function (data) {
                 var report = data.prospect_report.report;
-                // choose a random sector with cashola
-                var sector = Math.floor(Math.random() * report.length);
-                console.log("Deploying to sector",sector);
-                $.getJSON("deploy_hubs?token=3efbdfd5be1d284d8b3dd660cc31f839&hubs=H1&sector_ids=" + sector, function (data) {
+                // start with the first sector in the report
+                var topSector = report[0];
+                // loop thru each one
+                report.forEach(sector => {
+                    // replace the topSector if the ore volume is higher
+                    topSector = (sector[2]>topSector[2]) ? sector : topSector;
+                });
+                console.log("Deploying to sector",topSector[0]);
+                $.getJSON("deploy_hubs?token=3efbdfd5be1d284d8b3dd660cc31f839&hubs=H1&sector_ids=" + topSector[0], function (data) {
                     // you should see the hub appear
                     console.log("Deployed Hub H1");
                     // Wait for ore loading... dunno... 60 seconds?
-                    window.setTimeout(function () {
+                    window.setInterval(function () {
                         // deploy hub to 2000
-                        $.getJSON("ship_ore?token=3efbdfd5be1d284d8b3dd660cc31f839&hubs=H1&insured=true", function (data) {
+                        $.getJSON("ship_ore?token=3efbdfd5be1d284d8b3dd660cc31f839&hubs=H1&insured=false", function (data) {
                             // you should see the hub appear
                             console.log("Deployed Hub H1");
                         });
-                    }, 60000);
+                    }, 1000);
                 });
             });
         }, 3000); // 3 second delay before calling deploy
