01d2b15026cafd25f3797022b5ba86d7421c9874
Jeffrey Lee <somejeff@gmail.com>
2018-03-11 15:50:50 -0400

    #9 Risky Refactor without Tests!

--- mine-o-tron.js
+++ mine-o-tron.js
@@ -1,41 +1,82 @@
-// Reset Everything
-$.getJSON("startup?token=3efbdfd5be1d284d8b3dd660cc31f839", function (data) {
-    console.log("Week", data.startup.team.week);
-    // build a hub, you sould see our moneyh go down
-    $.getJSON("build_hubs?token=3efbdfd5be1d284d8b3dd660cc31f839&hubs=H1", function (data) {
-        // Wait... takes a few seconds
-        window.setTimeout(function () {
-            // deploy hub to 2000
-            $.getJSON("prospect_report?token=3efbdfd5be1d284d8b3dd660cc31f839", function (data) {
-                var report = data.prospect_report.report;
-                // start with the first sector in the report
-                var topSector = report[0];
-                // loop thru each one
-                report.forEach(sector => {
-                    // replace the topSector if the ore volume is higher
-                    topSector = (sector[2]>topSector[2]) ? sector : topSector;
-                });
-                console.log("Deploying to sector",topSector[0]);
-                $.getJSON("deploy_hubs?token=3efbdfd5be1d284d8b3dd660cc31f839&hubs=H1&sector_ids=" + topSector[0], function (data) {
-                    // you should see the hub appear
-                    console.log("Deployed Hub H1");
-                    // Wait for ore loading... dunno... 60 seconds?
-                    window.setInterval(function () {
-                        // deploy hub to 2000
-                        $.getJSON("ship_ore?token=3efbdfd5be1d284d8b3dd660cc31f839&hubs=H1&insured=false", function (data) {
-                            // you should see the hub appear
-                            console.log("Deployed Hub H1");
-                        });
-                    }, 1000);
-                });
-            });
-        }, 3000); // 3 second delay before calling deploy
-    });
-});
+// wrap it in closure to protect variables
+(function () {
+    // Global Status report
+    var statusReport = null;
 
-window.setInterval(function () {
-    $.getJSON("status_report?token=3efbdfd5be1d284d8b3dd660cc31f839", function (data) {
-        console.log(data.status_report.team.week);
-        console.table(data.status_report.hubs);
-    });
-}, 2000);
+    initialize();
+
+    buildHub();
+    window.setTimeout(deployHub,3000);
+    window.setInterval(shipOre,2000);
+
+    window.setInterval(fetchStatusReport, 2000);
+    window.setInterval(displayStatusReport, 2000);
+
+
+    /**
+     * Triggers the startup and resets everything
+     */
+    async function initialize() {
+        var data = await $.getJSON("startup?token=3efbdfd5be1d284d8b3dd660cc31f839").then();
+        statusReport = data.startup;
+    }
+
+    /** 
+     * Fetches the status Report and puts it in a global variable for everyone to use
+     * Not concerned with displaying the report
+     */
+    async function fetchStatusReport() {
+        var data = await $.getJSON("status_report?token=3efbdfd5be1d284d8b3dd660cc31f839").then();
+        statusReport = data.status_report;
+    }
+
+    /** 
+     * displays the current status report
+     * Currently using console log.
+     */
+    function displayStatusReport() {
+        console.log("Week:" + statusReport.team.week);
+        console.table(statusReport.hubs);
+    }
+
+
+    /** 
+     * Requests a new hub to be built.
+     * Currently hard coded ID. 
+     */
+    async function buildHub() {
+        var hubId = "H1"; // TODO random ID? UUID? Fetch cool names from the internet somewhere?
+        await $.getJSON("build_hubs?token=3efbdfd5be1d284d8b3dd660cc31f839&hubs=" + hubId).then();
+        console.log("Build Hub: " + hubId);
+    }
+
+    /** 
+     * Finds a sector and deploys a hub to that sector
+     */
+    async function deployHub() {
+        var hubId = "H1";
+        var data = await $.getJSON("prospect_report?token=3efbdfd5be1d284d8b3dd660cc31f839").then();
+        var report = data.prospect_report.report;
+        // start with the first sector in the report
+        var topSector = report[0];
+        // loop thru each one
+        report.forEach(sector => {
+            // replace the topSector if the ore volume is higher
+            topSector = (sector[2] > topSector[2]) ? sector : topSector;
+        });
+        console.log("Deploying " + hubId + " to sector", topSector[0]);
+        await $.getJSON("deploy_hubs?token=3efbdfd5be1d284d8b3dd660cc31f839&hubs=" + hubId + "&sector_ids=" + topSector[0]).then();
+        
+    }
+
+
+    /** 
+     * Ships Ore from a hub
+     */
+    async function shipOre() {
+        var hubId = "H1";
+        await $.getJSON("ship_ore?token=3efbdfd5be1d284d8b3dd660cc31f839&hubs=" + hubId + "&insured=false").then();
+        console.log("Shipping Ore from: " + hubId);
+    }
+
+})();
