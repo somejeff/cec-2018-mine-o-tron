8387915a20400825c68a727c8077e304279a0f4f
Jeffrey Lee <somejeff@gmail.com>
2018-03-11 14:35:03 -0400

    #6 Added the prospect report and picked any spot with $$$

--- mine-o-tron.js
+++ mine-o-tron.js
@@ -6,17 +6,23 @@ $.getJSON("startup?token=3efbdfd5be1d284d8b3dd660cc31f839", function (data) {
         // Wait... takes a few seconds
         window.setTimeout(function () {
             // deploy hub to 2000
-            $.getJSON("deploy_hubs?token=3efbdfd5be1d284d8b3dd660cc31f839&hubs=H1&sector_ids=2300", function (data) {
-                // you should see the hub appear
-                console.log("Deployed Hub H1");
-                // Wait for ore loading... dunno... 60 seconds?
-                window.setTimeout(function () {
-                    // deploy hub to 2000
-                    $.getJSON("ship_ore?token=3efbdfd5be1d284d8b3dd660cc31f839&hubs=H1&insured=true", function (data) {
-                        // you should see the hub appear
-                        console.log("Deployed Hub H1");
-                    });
-                }, 60000);
+            $.getJSON("prospect_report?token=3efbdfd5be1d284d8b3dd660cc31f839", function (data) {
+                var report = data.prospect_report.report;
+                // choose a random sector with cashola
+                var sector = Math.floor(Math.random() * report.length);
+                console.log("Deploying to sector",sector);
+                $.getJSON("deploy_hubs?token=3efbdfd5be1d284d8b3dd660cc31f839&hubs=H1&sector_ids=" + sector, function (data) {
+                    // you should see the hub appear
+                    console.log("Deployed Hub H1");
+                    // Wait for ore loading... dunno... 60 seconds?
+                    window.setTimeout(function () {
+                        // deploy hub to 2000
+                        $.getJSON("ship_ore?token=3efbdfd5be1d284d8b3dd660cc31f839&hubs=H1&insured=true", function (data) {
+                            // you should see the hub appear
+                            console.log("Deployed Hub H1");
+                        });
+                    }, 60000);
+                });
             });
         }, 3000); // 3 second delay before calling deploy
     });
