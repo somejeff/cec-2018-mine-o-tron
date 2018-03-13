91c3d131f0bcbf1f9f5687fc4702a4ace2dc1d60
Jeffrey Lee <jeffrey.lee@scotiabank.com>
2018-03-12 22:08:31 -0400

    #13 Big investment = Big returns

--- mine-o-tron.js
+++ mine-o-tron.js
@@ -4,13 +4,14 @@
     var statusReport = null;
     var parameters = null;
     var timers = [];
-
+    var hubCost = null;
+    var hubLocations = {};
     initialize();
 
     async function run() {
 
 
-        buildHub();
+        timers.push(window.setInterval(buildHub, 1000));
         timers.push(window.setInterval(shipOre, 2000));
         timers.push(window.setInterval(moveHub, Math.round((parameters.lifetime * parameters.ms_per_week) / 3)));  // move around 3 times
         timers.push(window.setInterval(fetchStatusReport, parameters.ms_per_week * 3)); // fetch every week
@@ -27,7 +28,7 @@
 
         data = await $.getJSON("parameters?token=3efbdfd5be1d284d8b3dd660cc31f839").then();
         parameters = data.parameters;
-
+        hubCost = parameters.costs.hub.rate + parameters.costs.deploy.rate + 100;
         console.log("Reboot");
         run();
     }
@@ -51,9 +52,9 @@
      * Currently using console log.
      */
     function displayStatusReport() {
-        console.log("Week:" + statusReport.team.week);
-        console.table(statusReport.hubs);
-        console.log(statusReport);
+        //console.log("Week:" + statusReport.team.week);
+        //console.table(statusReport.hubs);
+        //console.log(statusReport);
     }
 
     function gameOver() {
@@ -69,20 +70,24 @@
      * Currently hard coded ID. 
      */
     async function buildHub() {
-        var hubId = "H1"; // TODO random ID? UUID? Fetch cool names from the internet somewhere?
-        await $.getJSON("build_hubs?token=3efbdfd5be1d284d8b3dd660cc31f839&hubs=" + hubId).then();
-        console.log("Build Hub: " + hubId);
-        // deploy the hub as soon as it's ready
-        var delay = parameters.costs.hub.weeks * parameters.ms_per_week; // total delay in ms before hub can be deployed
-        window.setTimeout(deployHub, delay);
+        // only build a hub if we're in the first half of the lifetime, and there's enough moulah
+        if (statusReport.team.week < (parameters.lifetime * .50) && statusReport.team.balance > hubCost) {
+            var hubId = "H" + Math.floor(Math.random() * 100); // TODO random ID? UUID? Fetch cool names from the internet somewhere?
+            await $.getJSON("build_hubs?token=3efbdfd5be1d284d8b3dd660cc31f839&hubs=" + hubId).then();
+            console.log("Build Hub: " + hubId);
+            // deploy the hub as soon as it's ready
+            var delay = parameters.costs.hub.weeks * parameters.ms_per_week; // total delay in ms before hub can be deployed
+            window.setTimeout(function () { deployHub(hubId); }, delay);
+        }
     }
 
     /** 
      * Finds a sector and deploys a hub to that sector
      */
-    async function deployHub() {
-        var hubId = "H1";
+    async function deployHub(hubId) {
         var sectorID = await findTopSector();
+        // reserve the spot to avoid conflicts
+        hubLocations[hubId] = sectorID;
         console.log("Deploying " + hubId + " to sector", sectorID);
         await $.getJSON("deploy_hubs?token=3efbdfd5be1d284d8b3dd660cc31f839&hubs=" + hubId + "&sector_ids=" + sectorID).then();
 
@@ -93,10 +98,14 @@
      * Moves a hub if it's empty
      */
     async function moveHub() {
-        var hubId = "H1";
-        var sectorID = await findTopSector();
-        await $.getJSON("move_hubs?token=3efbdfd5be1d284d8b3dd660cc31f839&hubs=" + hubId + "&sector_ids=" + sectorID).then();
-        console.log("Moving Hub: " + hubId + " to sector " + sectorID);
+        Object.values(statusReport.hubs).forEach(async function (hub) {
+            var hubId = hub.hub_id;
+            var sectorID = await findTopSector();
+            // reserve the spot to avoid conflicts
+            hubLocations[hubId] = sectorID;
+            await $.getJSON("move_hubs?token=3efbdfd5be1d284d8b3dd660cc31f839&hubs=" + hubId + "&sector_ids=" + sectorID).then();
+            console.log("Moving Hub: " + hubId + " to sector " + sectorID);
+        });
     }
 
 
@@ -111,7 +120,9 @@
         // loop thru each one
         report.forEach(sector => {
             // replace the topSector if the ore volume is higher
-            topSector = (sector[2] > topSector[2]) ? sector : topSector;
+            if (sector[2] > topSector[2] && Object.values(hubLocations).indexOf(sector[0]) == -1) {
+                topSector = sector;
+            }
         });
         return topSector[0]; // return the sectorID of the top sector
     }
@@ -120,8 +131,8 @@
      * Ships Ore from a hub
      */
     async function shipOre() {
-        var hubId = "H1";
-        await $.getJSON("ship_ore?token=3efbdfd5be1d284d8b3dd660cc31f839&hubs=" + hubId + "&insured=false").then();
+        var hubIds = Object.keys(statusReport.hubs).join(',');
+        await $.getJSON("ship_ore?token=3efbdfd5be1d284d8b3dd660cc31f839&hubs=" + hubIds + "&insured=false").then();
     }
 
 })();
